7ee0550f078e57b8fa9671fef377361ea6a89258
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-12-21 15:24:58 +0200

    add

--- Product Manager/myNewProject/client/src/views/Update.js
+++ Product Manager/myNewProject/client/src/views/Update.js
@@ -1,77 +1,41 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import ProductForm from "../components/ProductForm";
+import DeleteButton from '../components/DeleteButton';
+
 
 const Update = (props) => {
-  const { id } = useParams();
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState();
-  const [description, setDescription] = useState("");
-  const navigate = useNavigate();
+const { id } = useParams();
+const [product, setProduct] = useState();
+const [loaded, setLoaded] = useState(false);
+const navigate =useNavigate();
 
-  useEffect(() => {
-    axios.get("http://localhost:8000/api/products/" + id).then((res) => {
-      setPrice(res.data.price);
-      setDescription(res.data.description);
-      setTitle(res.data.title);
-    });
-  }, []);
-  const updateProduct = (e) => {
-    e.preventDefault();
-    console.log("Updating");
-    axios
-      .put("http://localhost:8000/api/products/" + id, {
-        title,
-        price,
-        description,
-      })
-      .then((res) => navigate("/products"))
-      .catch((err) => console.error(err));
-  };
-  return (
+useEffect(() => {
+    axios.get('http://localhost:8000/api/products/' + id)
+        .then(res => {
+            setProduct(res.data);
+            setLoaded(true);
+        }).catch(err=>console.log(err))
+}, [ ]) 
+const updateProduct = product => {
+    axios.put('http://localhost:8000/api/products/'+id, product)
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+        navigate("/products/");
+}
+return (
     <div>
-      <h1>Update a Product</h1>
-      <form onSubmit={updateProduct}>
-        <p>
-          <label>Title</label>
-          <br />
-          <input
-            type="text"
-            name="title"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </p>
-        <p>
-          <label>Price</label>
-          <br />
-          <input
-            type="text"
-            name="price"
-            value={price}
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          />
-        </p>
-        <p>
-          <label>Description</label>
-          <br />
-          <input
-            type="text"
-            name="description"
-            value={price}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-        </p>
-        <input type="submit" />
-      </form>
+        <h2>Edit a Product</h2>
+        {loaded && (<>
+            <ProductForm handelSubmit={updateProduct}
+             initialTitle={product.title} 
+             initialPrice={product.price}
+            initialDescription={product.description}/>
+             <DeleteButton productId={product._id} successCallback={() => navigate("/products")} />
+        </>
+        )}
     </div>
-  );
-};
-
+)
+}
 export default Update;
